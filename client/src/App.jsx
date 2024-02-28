6d97194221f575dd418ab41483321c0436889361
Halil Barak <hbarak36@gmail.com>
2024-02-28 13:03:36 +0300

    create and add the footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Project from "./pages/Project"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 function App() {
 
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
