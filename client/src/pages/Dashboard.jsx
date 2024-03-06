cf0c34c4c31033c7117c85bc72f28991f520ee9d
Halil Barak <hbarak@gmail.com>
2024-03-06 15:32:55 +0300

    compilete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "./DashSidebar"
+import DashProfile from "./DashProfile"
+
 
 function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState("")
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFormUrl = urlParams.get("tab")
+    if (tabFormUrl) {
+      setTab(tabFormUrl)
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/*sidebar*/}
+      <div className="md:w-56">
+        <DashSidebar/>
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <DashProfile/>}
+    </div>
+
   )
 }
 
